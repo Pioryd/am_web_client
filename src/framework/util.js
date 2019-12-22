757872982d7f5698b37f92ab0698f32c4d3777be
Piotr Rydwański <pioryd123@gmail.com>
2019-12-22 16:51:58 +0100

    Util add support for custom date

--- src/framework/util.js
+++ src/framework/util.js
@@ -1,6 +1,7 @@
 class Util {
-  static get_time_hms() {
-    return new Date().toLocaleTimeString("en-US", {
+  static get_time_hms(date) {
+    if (date == null) date = new Date();
+    return date.toLocaleTimeString("en-US", {
       hour12: false,
       hour: "numeric",
       minute: "numeric",
