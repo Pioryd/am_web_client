f245b516d61fcc2b319f837211cd41072f2507fb
Piotr Rydwański <pioryd123@gmail.com>
2020-01-19 19:49:05 +0100

    Add possibility to move character by keys A and D

--- src/components/content/windows/pui/graphical_ui/index.js
+++ src/components/content/windows/pui/graphical_ui/index.js
@@ -23,7 +23,8 @@ function GraphicalUI() {
   const {
     context_character_data_character,
     context_character_data_land,
-    context_character_data_world
+    context_character_data_world,
+    context_character_send_change_position
   } = React.useContext(AppContext);
 
   const [state_map_size, set_state_map_size] = React.useState(12);
@@ -32,12 +33,16 @@ function GraphicalUI() {
     set_state_character_div_style
   ] = React.useState({
     top: "-230px",
-    left: 70 * 1 + "px",
+    left: CELL_SIZE * 1 + "px",
     width: "66px",
     height: "92px",
     backgroundImage: "url(/images/game/Player/p3_front.png)"
   });
   const [state_cells, set_state_cells] = React.useState([]);
+  const [
+    state_current_index_position,
+    set_state_current_index_position
+  ] = React.useState(-1);
 
   const render_map = (map, environment_objects) => {
     let cells = [];
@@ -110,19 +115,50 @@ function GraphicalUI() {
     set_state_cells(cells);
   };
 
+  const render_character = current_index_position => {
+    let style = { ...state_character_div_style };
+
+    const left = CELL_SIZE * (current_index_position + 1);
+    style.left = left + "px";
+    set_state_character_div_style(style);
+  };
+
+  const on_key_press = e => {
+    if (!("map" in context_character_data_land)) return;
+
+    let index_position = state_current_index_position;
+    const max_index_position = context_character_data_land.map.length - 1;
+    if (e.key === "a") index_position = Math.max(0, index_position - 1);
+    else if (e.key === "d")
+      index_position = Math.min(max_index_position, index_position + 1);
+    else return;
+
+    context_character_send_change_position({ position_x: index_position });
+  };
+
   React.useEffect(() => {
     const land_data = context_character_data_land;
+    const world_data = context_character_data_world;
+    const character_data = context_character_data_character;
     if (
       "map" in land_data &&
-      "environment_objects_map" in context_character_data_world
+      "environment_objects_map" in world_data &&
+      "id" in character_data
     ) {
+      const character_id = character_data.id;
       const map_size = land_data.map.length;
+      let current_index_position = -1;
+      for (let i = 0; i < land_data.map.length; i++) {
+        if (land_data.map[i].characters_list.includes(character_id)) {
+          current_index_position = i;
+          break;
+        }
+      }
 
+      set_state_current_index_position(current_index_position);
       set_state_map_size(map_size);
-      render_map(
-        land_data.map,
-        context_character_data_world.environment_objects_map
-      );
+      render_character(current_index_position);
+      render_map(land_data.map, world_data.environment_objects_map);
     }
   }, [
     context_character_data_character,
@@ -132,7 +168,7 @@ function GraphicalUI() {
 
   return (
     <React.Fragment>
-      <div className="content_body">
+      <div className="content_body" onKeyPress={on_key_press} tabIndex="0">
         <div className="bar"></div>
         <div className="graphical_ui">
           <div className="content">
