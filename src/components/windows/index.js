ae16fa4954e99c69836719685301d5643191b7b6
Piotr Rydwański <pioryd123@gmail.com>
2020-03-06 23:04:29 +0100

    Rebuild windows adding

---
+++ src/components/windows/index.js
@@ -0,0 +1,7 @@
+import Settings from "./settings";
+
+export default {
+  windows_map: {
+    client_settings: { class: Settings, title: "Client Settings" }
+  }
+};
--- src/modules/admin/index.js
+++ src/modules/admin/index.js
@@ -4,12 +4,12 @@ import ModuleData from "./components/module_data";
 import RunScript from "./components/run_script";
 import ScriptsList from "./components/scripts_list";
 
-const windows_map = {
-  form_editor: { class: FormEditor, title: "Form editor" },
-  code_editor: { class: CodeEditor, title: "Code editor" },
-  module_data: { class: ModuleData, title: "Module data" },
-  admin_run_script: { class: RunScript, title: "Run script" },
-  admin_scripts_list: { class: ScriptsList, title: "Scripts list" }
+export default {
+  windows_map: {
+    form_editor: { class: FormEditor, title: "Form editor" },
+    code_editor: { class: CodeEditor, title: "Code editor" },
+    module_data: { class: ModuleData, title: "Module data" },
+    admin_run_script: { class: RunScript, title: "Run script" },
+    admin_scripts_list: { class: ScriptsList, title: "Scripts list" }
+  }
 };
-
-export default { windows_map };
