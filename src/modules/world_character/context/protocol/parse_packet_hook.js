53554ad0f059efcc7a689742eb84a2fe6d771ec7
Piotr Rydwański <pioryd123@gmail.com>
2020-03-06 16:19:06 +0100

    Rebuild protocol

--- src/modules/world_character/context/protocol/parse_packet_hook.js
+++ src/modules/world_character/context/protocol/parse_packet_hook.js
@@ -1,98 +1,45 @@
-import React from "react";
+import usePacketManagerHook from "./../../../../hooks/packet_manager";
 
 function useParsePacketHook(props) {
-  const ref_client = React.useRef();
-  const [state_logged_as, set_state_logged_as] = React.useState("");
-  const [state_packets_data, set_state_packets_data] = React.useState({});
-
-  const get_client = () => {
-    return ref_client.current;
-  };
-
-  const send = (packet_id, data) => {
-    if (get_client() == null) return;
-
-    if (
-      packet_id !== "accept_connection" &&
-      get_client().ext.logged_in !== true
-    )
-      return;
-
-    get_client().send(packet_id, data);
-  };
-
-  const packets_pop = packet_id => {
-    const packets_data = { ...state_packets_data };
-
-    if (!(packet_id in packets_data)) return [];
-    if (packets_data[packet_id].length === 0) return [];
-
-    const packet_data_list = packets_data[packet_id];
-    packets_data[packet_id] = [];
-
-    set_state_packets_data(packets_data);
-
-    return packet_data_list;
-  };
-
-  const packets_peek = packet_id => {
-    const packets_data = { ...state_packets_data };
-
-    if (!(packet_id in packets_data)) return [];
-    if (packets_data[packet_id].length === 0) return [];
-
-    const packet_data_list = packets_data[packet_id];
-
-    return JSON.parse(JSON.stringify(packet_data_list));
-  };
-
-  const _push_packet_data = (packet_id, data) => {
-    const packets_data = { ...state_packets_data };
-
-    if (!(packet_id in packets_data)) packets_data[packet_id] = [];
-
-    while (packets_data[packet_id].length >= 10)
-      packets_data[packet_id].shift();
-
-    packets_data[packet_id].push(data);
-
-    set_state_packets_data(packets_data);
-  };
+  const {
+    hook_packets_data,
+    hook_packets_fn,
+    hook_ref_client
+  } = usePacketManagerHook();
 
   return {
     hook_parse_packet: {
-      login: data => {
-        set_state_logged_as(data.character_name);
+      accept_connection: data => {
+        hook_packets_fn._push("accept_connection", data);
 
-        get_client().ext.logged_in = true;
+        hook_ref_client.current.ext.logged_in = true;
 
-        send("data_character", {});
-        send("data_land", {});
-        send("data_world", {});
+        hook_packets_fn.send("data_character", {});
+        hook_packets_fn.send("data_land", {});
+        hook_packets_fn.send("data_world", {});
       },
       data_character: data => {
-        _push_packet_data("data_character", data);
-        send("data_character", {});
+        hook_packets_fn._push("data_character", data);
+        hook_packets_fn.send("data_character", {});
       },
       data_land: data => {
-        _push_packet_data("data_land", data);
-        send("data_land", {});
+        hook_packets_fn._push("data_land", data);
+        hook_packets_fn.send("data_land", {});
       },
       data_world: data => {
-        _push_packet_data("data_world", data);
-        send("data_world", {});
+        hook_packets_fn._push("data_world", data);
+        hook_packets_fn.send("data_world", {});
       },
       action_message: data => {
-        _push_packet_data("action_message", data);
+        hook_packets_fn._push("action_message", data);
       },
       virtual_world: data => {
-        _push_packet_data("virtual_world", data);
+        hook_packets_fn._push("virtual_world", data);
       }
     },
-    hook_logged_as: state_logged_as,
-    hook_packets_data: state_packets_data,
-    hook_packets_fn: { pop: packets_pop, peek: packets_peek },
-    hook_ref_client: ref_client
+    hook_packets_data,
+    hook_packets_fn,
+    hook_ref_client
   };
 }
 
