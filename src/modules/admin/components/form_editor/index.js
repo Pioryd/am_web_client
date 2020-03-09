819ff10772b7c826b227adb99ac68de114578aa1
Piotr Rydwański <pioryd123@gmail.com>
2020-03-09 10:19:35 +0100

    Split for editor to protocol hook

--- src/modules/admin/components/form_editor/index.js
+++ src/modules/admin/components/form_editor/index.js
@@ -1,95 +1,27 @@
 import React from "react";
 import Select from "react-select";
 import { JsonTree } from "react-editable-json-tree";
-import Util from "../../../../framework/util";
-import { ProtocolContext } from "../../../../context/protocol";
+
 import useEditJson from "./edit_json_hook";
+import useProtocolHook from "./protocol_hook";
 
 function FormEditor() {
-  const { context_packets_data, context_packets_fn } = React.useContext(
-    ProtocolContext
-  );
-
   const {
-    hook_last_message,
+    hook_edit_json_last_log,
     hook_current_form,
     hook_data_changed,
     hook_edit_json_fn
   } = useEditJson();
 
-  const [state_action_id, set_state_action_id] = React.useState("");
+  const {
+    hook_forms,
+    hook_protocol_last_log,
+    hook_protocol_action_id,
+    hook_protocol_fn
+  } = useProtocolHook();
 
   const [state_options, set_state_options] = React.useState([]);
   const [state_selected_option, set_state_selected_option] = React.useState("");
-
-  const [state_last_message, set_state_last_message] = React.useState("");
-
-  const update_last_message = message => {
-    set_state_last_message(`"[${Util.get_time_hms()}] ${message}"`);
-  };
-
-  const can_perform_action = () => {
-    if (state_action_id !== "") {
-      update_last_message(
-        "You cannot perform action. Until last one will done."
-      );
-      return false;
-    }
-    return true;
-  };
-
-  const get_forms = () => {
-    if (can_perform_action() === false) return;
-
-    const action_id = Date.now();
-
-    context_packets_fn.send("data_am_form", { action_id });
-    set_state_action_id(action_id);
-  };
-
-  const new_form = () => {
-    if (can_perform_action() === false) return;
-
-    const action_id = Date.now();
-
-    context_packets_fn.send("update_am_form", {
-      action_id,
-      id: "",
-      object: null
-    });
-
-    get_forms();
-  };
-
-  const save_form = () => {
-    if (can_perform_action() === false) return;
-    if (!("id" in hook_current_form)) return;
-
-    const action_id = Date.now();
-
-    context_packets_fn.send("update_am_form", {
-      action_id,
-      id: hook_current_form.id,
-      object: hook_current_form
-    });
-
-    get_forms();
-  };
-
-  const remove_form = () => {
-    if (can_perform_action() === false) return;
-    if (!("id" in hook_current_form)) return;
-
-    const action_id = Date.now();
-
-    context_packets_fn.send("update_am_form", {
-      action_id,
-      id: hook_current_form.id,
-      object: null
-    });
-
-    get_forms();
-  };
 
   // Handle select
   React.useEffect(() => {
@@ -101,66 +33,74 @@ function FormEditor() {
       if (option.label === label) form = option.value;
     }
 
-    update_last_message(
-      `Changed from[${hook_current_form.name}] to[${form.name}]`
-    );
     hook_edit_json_fn.set_current_form(form);
   }, [state_selected_option]);
 
   // Parse packet
   React.useEffect(() => {
-    const data_am_form = context_packets_fn.pop("data_am_form");
-
-    if (state_action_id === "") return;
-
     let options = null;
     let current_form = null;
-    let action_id = null; // For searching needs
     let selected_option = null;
 
-    // Search for action_id
-    for (const data of data_am_form) {
-      if (data.action_id === state_action_id) {
-        options = [];
-        current_form = {};
-        action_id = state_action_id;
-        selected_option = "";
+    options = [];
+    current_form = {};
 
-        for (const form of Object.values(data.forms))
-          options.push({ label: form.id + "_" + form.name, value: form });
+    selected_option = "";
 
-        if ("id" in hook_current_form) {
-          for (const option of options) {
-            const form = option.value;
+    for (const form of Object.values(hook_forms))
+      options.push({ label: form.id + "_" + form.name, value: form });
 
-            if (form.id === hook_current_form.id) {
-              selected_option = option;
-              current_form = form;
-              break;
-            }
-          }
+    if ("id" in hook_current_form) {
+      for (const option of options) {
+        const form = option.value;
+
+        if (form.id === hook_current_form.id) {
+          selected_option = option;
+          current_form = form;
+          break;
         }
-
-        break;
       }
     }
 
     if (options != null) set_state_options(options);
     if (current_form != null) hook_edit_json_fn.set_current_form(current_form);
-    if (action_id != null) set_state_action_id("");
     if (selected_option != null) set_state_selected_option(selected_option);
-  }, [context_packets_data]);
+  }, [hook_forms]);
 
   return (
     <div className="content_body">
       <div className="bar">
-        <button onClick={get_forms}>refresh</button>
-        <button onClick={new_form}>new</button>
-        <button onClick={save_form}>save</button>
-        <button onClick={remove_form}>remove</button>
+        <button
+          onClick={() => {
+            hook_protocol_fn.get_forms(hook_current_form);
+          }}
+        >
+          refresh
+        </button>
+        <button
+          onClick={() => {
+            hook_protocol_fn.new_form(hook_current_form);
+          }}
+        >
+          new
+        </button>
+        <button
+          onClick={() => {
+            hook_protocol_fn.save_form(hook_current_form);
+          }}
+        >
+          save
+        </button>
+        <button
+          onClick={() => {
+            hook_protocol_fn.remove_form(hook_current_form);
+          }}
+        >
+          remove
+        </button>
       </div>
       <div className="bar">
-        <label>{state_last_message}</label>
+        <label>{hook_protocol_last_log}</label>
       </div>
       <div>
         <Select
@@ -173,7 +113,7 @@ function FormEditor() {
         />
       </div>
       <div className="bar">
-        <label>{hook_last_message}</label>
+        <label>{hook_edit_json_last_log}</label>
       </div>
       <div className="form_editor">
         {hook_data_changed === true && (
@@ -181,12 +121,12 @@ function FormEditor() {
             <label style={{ color: "red" }}>Save to apply changes</label>
           </div>
         )}
-        {state_action_id !== "" ? (
+        {hook_protocol_action_id !== "" ? (
           <React.Fragment>
-            <p>{`Waiting for the end of the action[${state_action_id}]`}</p>
+            <p>{`Waiting for the end of the action[${hook_protocol_action_id}]`}</p>
             <button
               onClick={() => {
-                set_state_action_id("");
+                hook_protocol_fn.cancel_action();
               }}
             >
               cancel action
