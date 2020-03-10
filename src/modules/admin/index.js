3d4f4e10b7ea565c63786c312423fae23a3f5f9b
Piotr Rydwański <pioryd123@gmail.com>
2020-03-10 23:58:38 +0100

    Move code editor to components

--- src/modules/admin/index.js
+++ src/modules/admin/index.js
@@ -1,5 +1,4 @@
 import FormEditor from "./windows/form_editor";
-import CodeEditor from "./windows/code_editor";
 import ModuleData from "./windows/module_data";
 import RunScript from "./windows/run_script";
 import ScriptsList from "./windows/scripts_list";
@@ -7,7 +6,6 @@ import ScriptsList from "./windows/scripts_list";
 export default {
   windows_map: {
     form_editor: { class: FormEditor, title: "Form editor" },
-    code_editor: { class: CodeEditor, title: "Code editor" },
     module_data: { class: ModuleData, title: "Module data" },
     admin_run_script: { class: RunScript, title: "Run script" },
     admin_scripts_list: { class: ScriptsList, title: "Scripts list" }
