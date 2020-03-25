507f43a99565450b2c424e64083572db79adefbf
Piotr Rydwański <pioryd123@gmail.com>
2020-03-25 17:46:41 +0100

    Add module_info to admin module

---
+++ src/modules/admin/windows/module_info.js
@@ -0,0 +1,8 @@
+import React from "react";
+import JsonData from "../components/json_data";
+
+function ModuleInfo() {
+  return <JsonData packet_name="module_info" />;
+}
+
+export default ModuleInfo;
