7aa2f5e256629abbb8e42f7dcbe16778ef46f1c1
Piotr Rydwański <pioryd123@gmail.com>
2020-03-31 16:06:41 +0200

    Update validator

--- src/modules/admin/components/am_json_editor/validate_hook.js
+++ src/modules/admin/components/am_json_editor/validate_hook.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Util from "../../../../framework/util";
-import Validator from "../../../../framework/validator";
+import Ajv from "ajv";
 
 function useValidate(props) {
-  const [state_validator, set_state_validator] = React.useState(
-    new Validator({})
-  );
+  const [state_rules, set_state_rules] = React.useState({});
   const [state_last_error, set_state_last_error] = React.useState("Initialize");
 
   const update_last_error = message => {
@@ -16,15 +14,17 @@ function useValidate(props) {
   return {
     hook_validate_last_error: state_last_error,
     hook_validate_fn: {
-      set_rules: rules => set_state_validator(new Validator(rules)),
+      set_rules: rules => set_state_rules(rules),
       validate: object => {
-        try {
-          state_validator.validate(Util.shallow_copy(object));
-          return true;
-        } catch (e) {
-          update_last_error(e);
+        const ajv = new Ajv({ allErrors: true });
+        const validate = ajv.compile(state_rules);
+        const valid = validate(Util.shallow_copy(object));
+
+        if (!valid) {
+          update_last_error("AJV:" + ajv.errorsText(validate.errors));
           return false;
         }
+        return true;
       }
     }
   };
