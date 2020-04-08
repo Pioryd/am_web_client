2f05cda649efc5afb70e3746d42c28af9a9aa6df
Piotr Rydwański <pioryd123@gmail.com>
2020-04-08 14:56:07 +0200

    Update json_data domponent

--- src/modules/admin/windows/module_info.js
+++ src/modules/admin/windows/module_info.js
@@ -2,7 +2,16 @@ import React from "react";
 import JsonData from "../components/json_data";
 
 function ModuleInfo() {
-  return <JsonData packet_name="module_info" />;
+  return (
+    <div className="content_body">
+      <JsonData
+        packet_name="module_info"
+        auto_sync={true}
+        refresh={true}
+        clear={false}
+      />
+    </div>
+  );
 }
 
 export default ModuleInfo;
