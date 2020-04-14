c1dc312907bcd560b412123934276f10550ea2b1
Piotr Rydwański <pioryd123@gmail.com>
2020-04-15 01:53:41 +0200

    Update protocols of module admin and world_character

--- src/modules/admin/hooks/parse_packet.js
+++ src/modules/admin/hooks/parse_packet.js
@@ -7,32 +7,23 @@ function useParsePacketHook(props) {
     hook_ref_client
   } = usePacketManagerHook();
 
+  const parse_packets = {
+    accept_connection: data => {
+      hook_packets_fn._push("accept_connection", data);
+
+      hook_ref_client.current.ext.logged_in = true;
+
+      hook_packets_fn.send("scripts_list", {});
+    }
+  };
+
   return {
     hook_parse_packet: {
-      accept_connection: data => {
-        hook_packets_fn._push("accept_connection", data);
-
-        hook_ref_client.current.ext.logged_in = true;
-
-        hook_packets_fn.send("scripts_list", {});
-      },
-      module_data: data => hook_packets_fn._push("module_data", data),
-      module_info: data => hook_packets_fn._push("module_info", data),
-      data_admin_script: data =>
-        hook_packets_fn._push("data_admin_script", data),
-      data_am_form: data => hook_packets_fn._push("data_am_form", data),
-      data_am_program: data => hook_packets_fn._push("data_am_program", data),
-      data_am_system: data => hook_packets_fn._push("data_am_system", data),
-      data_am_script: data => hook_packets_fn._push("data_am_script", data),
-      update_admin_script: data =>
-        hook_packets_fn._push("update_admin_script", data),
-      update_am_form: data => hook_packets_fn._push("update_am_form", data),
-      update_am_program: data =>
-        hook_packets_fn._push("update_am_program", data),
-      update_am_system: data => hook_packets_fn._push("update_am_system", data),
-      update_am_script: data => hook_packets_fn._push("update_am_script", data),
-      process_admin_script: data =>
-        hook_packets_fn._push("process_admin_script", data)
+      root: data => {
+        const { packet_id, packet_data } = data;
+        if (packet_id in parse_packets) parse_packets[packet_id](packet_data);
+        else hook_packets_fn._push(packet_id, packet_data);
+      }
     },
     hook_packets_data,
     hook_packets_fn,
--- src/modules/world_character/hooks/parse_packet.js
+++ src/modules/world_character/hooks/parse_packet.js
@@ -7,36 +7,42 @@ function useParsePacketHook(props) {
     hook_ref_client
   } = usePacketManagerHook();
 
+  const parse_packets = {
+    accept_connection: data => {
+      hook_packets_fn._push("accept_connection", data);
+      const { characters_info, am_data } = data;
+      const character_id = Object.values(characters_info)[0].id;
+      hook_ref_client.current.ext.logged_in = true;
+      hook_ref_client.current.ext.character_id = character_id;
+
+      hook_packets_fn.send("data_character", { character_id });
+      hook_packets_fn.send("data_land", { character_id });
+      hook_packets_fn.send("data_world", { character_id });
+    },
+    data_character: data => {
+      hook_packets_fn._push("data_character", data);
+      const { character_id } = hook_ref_client.current.ext;
+      hook_packets_fn.send("data_character", { character_id });
+    },
+    data_land: data => {
+      hook_packets_fn._push("data_land", data);
+      const { character_id } = hook_ref_client.current.ext;
+      hook_packets_fn.send("data_land", { character_id });
+    },
+    data_world: data => {
+      hook_packets_fn._push("data_world", data);
+      const { character_id } = hook_ref_client.current.ext;
+      hook_packets_fn.send("data_world", { character_id });
+    }
+  };
+
   return {
     hook_parse_packet: {
-      accept_connection: data => {
-        hook_packets_fn._push("accept_connection", data);
-        const { characters_info, am_data } = data;
-        const character_id = Object.values(characters_info)[0].id;
-        hook_ref_client.current.ext.logged_in = true;
-        hook_ref_client.current.ext.character_id = character_id;
-
-        hook_packets_fn.send("data_character", { character_id });
-        hook_packets_fn.send("data_land", { character_id });
-        hook_packets_fn.send("data_world", { character_id });
-      },
-      data_character: data => {
-        hook_packets_fn._push("data_character", data);
-        const { character_id } = hook_ref_client.current.ext;
-        hook_packets_fn.send("data_character", { character_id });
-      },
-      data_land: data => {
-        hook_packets_fn._push("data_land", data);
-        const { character_id } = hook_ref_client.current.ext;
-        hook_packets_fn.send("data_land", { character_id });
-      },
-      data_world: data => {
-        hook_packets_fn._push("data_world", data);
-        const { character_id } = hook_ref_client.current.ext;
-        hook_packets_fn.send("data_world", { character_id });
-      },
-      action_message: data => hook_packets_fn._push("action_message", data),
-      virtual_world: data => hook_packets_fn._push("virtual_world", data)
+      root: data => {
+        const { packet_id, packet_data } = data;
+        if (packet_id in parse_packets) parse_packets[packet_id](packet_data);
+        else hook_packets_fn._push(packet_id, packet_data);
+      }
     },
     hook_packets_data,
     hook_packets_fn,
