50e79f7416648ceb227bacd40990d8b8b01e714f
Piotr Rydwański <pioryd123@gmail.com>
2020-05-05 18:41:30 +0200

    Update util.js

--- src/framework/util.js
+++ src/framework/util.js
@@ -1,5 +1,5 @@
-class Util {
-  static get_time_hms(date) {
+export default {
+  get_time_hms: (date) => {
     if (date == null) date = new Date();
     return date.toLocaleTimeString("en-US", {
       hour12: false,
@@ -7,11 +7,31 @@ class Util {
       minute: "numeric",
       second: "numeric"
     });
-  }
-
-  static shallow_copy(object) {
+  },
+  shallow_copy: (object) => {
     return JSON.parse(JSON.stringify(object));
-  }
-}
+  },
+  command_args_to_array: (text) => {
+    const re = /^"[^"]*"$/; // Check if argument is surrounded with double-quotes
+    const re2 = /^([^"]|[^"].*?[^"])$/; // Check if argument is NOT surrounded with double-quotes
 
-export default Util;
+    let arr = [];
+    let argPart = null;
+
+    text &&
+      text.split(" ").forEach(function (arg) {
+        if ((re.test(arg) || re2.test(arg)) && !argPart) {
+          arr.push(arg);
+        } else {
+          argPart = argPart ? argPart + " " + arg : arg;
+          // If part is complete (ends with a double quote), we can add it to the array
+          if (/"$/.test(argPart)) {
+            arr.push(argPart);
+            argPart = null;
+          }
+        }
+      });
+
+    return arr;
+  }
+};
