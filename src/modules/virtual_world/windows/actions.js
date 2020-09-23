f497d218d283f61ae3dc82fb4a741aeef3f6bc63
Piotr Rydwański <pioryd123@gmail.com>
2020-09-23 15:05:34 +0200

    Update - module - virtual_world - keep windows as pure components

---
+++ src/modules/virtual_world/windows/actions.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Actions from "../components/actions";
+
+function ActionsWindow(props) {
+  return <Actions {...props} />;
+}
+
+export default ActionsWindow;
---
+++ src/modules/virtual_world/windows/api.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Api from "../components/api";
+
+function ApiWindow(props) {
+  return <Api {...props} />;
+}
+
+export default ApiWindow;
