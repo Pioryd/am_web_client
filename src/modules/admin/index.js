34e72b55ceb9ec05df56a0586aba90bb5ccd3eda
Piotr Rydwański <pioryd123@gmail.com>
2020-10-16 18:10:00 +0200

    Remove - module - admin - visual_connections

--- src/modules/admin/index.js
+++ src/modules/admin/index.js
@@ -1,17 +1,12 @@
 import DataEditor from "./windows/data_editor";
 import ModuleData from "./windows/module_data";
 import ModuleInfo from "./windows/module_info";
-import VisualConnections from "./windows/visual_connections";
 
 export default {
   windows: {
     data_editor: { class: DataEditor, title: "Data editor" },
     module_data: { class: ModuleData, title: "Module data" },
-    module_info: { class: ModuleInfo, title: "Module info" },
-    visual_connections: {
-      class: VisualConnections,
-      title: "Visual Connections"
-    }
+    module_info: { class: ModuleInfo, title: "Module info" }
   },
   gui_type: "multi_window",
   settings: {}
--- src/modules/admin/windows/visual_connections.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import VisualConnections from "../components/visual_connections";
-
-function VisualConnectionsWindow(props) {
-  return <VisualConnections {...props} />;
-}
-
-export default VisualConnectionsWindow;
